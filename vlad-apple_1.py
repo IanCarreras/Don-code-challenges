02788dd5bc232d77fb41d65a1f2052bc42b71b41
Ian <mus5002@aol.com>
2020-07-10 12:58:35 -0400

    added new challenge

---
+++ vlad-apple_1.py
@@ -0,0 +1,19 @@
+# Given a sorted array, convert it into a binary search tree.
+# Can you do this both recursively and iteratively?
+
+class Node(object):
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return f'{self.val}, ({self.left}, {self.right})'
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        return
+
+n = Solution().sortedArrayToBST([-10, -3, 0, 5, 9])
+print(n)
+# 0, (-3, (-10, (None, None), None), 9, (5, (None, None), None))
