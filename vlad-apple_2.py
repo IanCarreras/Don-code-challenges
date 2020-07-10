cac5ed8634b1e6e91d5aa7c6e26af245f50f9ce1
Ian <mus5002@aol.com>
2020-07-10 13:10:14 -0400

    added new challenge

---
+++ vlad-apple_2.py
@@ -0,0 +1,12 @@
+# Given an array of numbers, determine whether it can be partitioned into 3 arrays of equal sums.
+# For instance,
+# [0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1] can be partitioned into:
+# [0, 2, 1], [-6, 6, -7, 9, 1], [2, 0, 1] all of which sum to 3.
+
+class Solution(object):
+    def canThreePartsEqualSum(self, a):
+        return
+
+print(Solution().canThreePartsEqualSum(
+    [0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1]
+))
