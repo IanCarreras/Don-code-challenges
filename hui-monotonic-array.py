c332a193512d6823e8432e721a69ae8f503ff56c
Ian <mus5002@aol.com>
2020-08-05 18:27:28 -0400

    Hui challenge, monotonic array

---
+++ hui-monotonic-array.py
@@ -0,0 +1,9 @@
+# write a function that takes in an array of integers and returns a boolean 
+# representing whether the array is monotonic
+
+# an array is said to be monotonic if it's elements, from left to right, are
+# entirely non-increasing or entirely non-decreasing
+
+array = [-1, -5, -10, -1100, -1101, -1102, -9001]
+
+# outpu: true
