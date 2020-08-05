0e89e54b0bf9711da8daa80137fe2b6889e6d678
Ian <mus5002@aol.com>
2020-08-05 18:30:48 -0400

    Hui challenge, find three largest numbers

---
+++ hui-three-largest-numbers.py
@@ -0,0 +1,10 @@
+# write a function that takes in an array of at least three integers and, without
+# sorting the input array, returns a sorted array of the three largest integers
+# in the input array
+
+# the function should return duplicate integers if necessary; for example, it should
+# return [10, 10, 12] for an input array of [10, 5, 9, 10, 12]
+
+array = [141, 1, 17, -7, -27, 18, 541, 8, 7, 7]
+
+# output: [18, 141, 541]
